3ce934ffc45a054930d09c79237d2cb268e190c5
DanielAdmin <prodevildaniel@gmail.com>
2022-06-17 03:01:51 +0300

    added comments

--- bot.py
+++ bot.py
@@ -1,3 +1,15 @@
+# YoutubeDownloadApp Bot version 1
+#   this bot is designed to receive a string from user input
+#   then search a video/s on YouTube matching the string
+#   then download the video/s(if not already exists in 'ytdlAppData' directory), while based on predefined limitations for guaranteeing performance
+#   then send the video/s to user
+#   standby for new user input
+# Parameters:
+#
+# ?help (?help): display help menu
+# /mnr: X (/mnr: X): display the settings menu, for more information, can review help menu
+
+
 from telegram.ext import Updater, MessageHandler, Filters
 from utils import search_download_youtube_video
 from loguru import logger
@@ -45,55 +57,67 @@ class QuoteBot(Bot):
 class YoutubeBot(Bot):
 
     def _message_handler(self, incoming_msg, context):
+        # Define number of results based on global variable
         global num_results
+        # Define user input into variable
         inbound_text = incoming_msg.message.text
+        # Check if the user is trying to use the help menu
         if inbound_text.startswith('?'):
             if inbound_text == '?help':
+                # Check if the user is trying to use the help menu and respond to correct input with relevant information
                 self.send_text(incoming_msg, f'Welcome to Nicely Done Bot, This bot is simple, enter a text to search on youtube, the bot will download the video/s according to '
                                              f'defined max number of results and send you the results via this chat.')
+                # Show information on Settings mode
                 sleep(1)
                 self.send_text(incoming_msg,
-                               f'Settings mode: \n To change max number of results, type "/mnr: X" while replacing the "X" with the max number of results you would like \n Due '
+                               f'Settings mode: \nTo change max number of results, type "/mnr: X" while replacing the "X" with the max number of results you would like \nDue '
                                f'to system limitations, available values are between 1-5')
-                sleep(1)
                 self.send_text(incoming_msg, f'Current max number of results is {num_results}')
             else:
+                # Respond with example syntax for help menu
                 self.send_text(incoming_msg, 'Please use the correct command for this syntax: "?help"')
                 sleep(1)
         else:
+            # Check if the user is trying to use the Settings menu
             if inbound_text.startswith('/'):
                 if inbound_text.startswith('/mnr:'):
+                    # Extract selected value from user input for verification
                     ivar = inbound_text[inbound_text.rindex(':') + 1:]
-                    ivar.strip()
-                    ivar = int(ivar)
-                    self.send_text(incoming_msg, f'Settings mode detected, will attempt to update max number of results(1-5): {inbound_text}')
-                    sleep(1)
-                    self.send_text(incoming_msg, f'Attempting to change number of results to: {ivar}')
-                    sleep(1)
-                    if isinstance(ivar, int) & (1 <= ivar <= 5):
+                    ivar = ivar.strip()
+                    allowed_values = ["1", "2", "3", "4", "5"]
+                    # Verifying user input for settings mode
+                    if ivar not in allowed_values:
+                        self.send_text(incoming_msg, 'Please use the correct command for this syntax, type "?help" to get more information.')
+                        sleep(1)
+                    else:
+                        # Convert to type int
+                        ivar = int(ivar)
+                        self.send_text(incoming_msg, f'Settings mode detected, will attempt to update max number of results(1-5): {inbound_text}')
+                        sleep(1)
+                        self.send_text(incoming_msg, f'Attempting to change number of results to: {ivar}')
+                        sleep(1)
+                        # Try to update max number of results
                         try:
                             num_results = ivar
                             self.send_text(incoming_msg, f'Successfully Changed to: {ivar}')
                         except:
                             self.send_text(incoming_msg, f'Unable to comply, an error has occured.')
                             print("An exception occurred")
-                    else:
-                        self.send_text(incoming_msg, 'Please use the correct command for this syntax, type "?help" to get more information.')
-                        sleep(1)
                 else:
                     self.send_text(incoming_msg, 'Please use the correct command for this syntax, type "?help" to get more information.')
                     sleep(1)
             else:
-                self.send_text(incoming_msg, f'Your searched for the following string: {inbound_text}')
+                # Continue to youtube download mode
+                self.send_text(incoming_msg, f'Your searched for the following: {inbound_text}')
                 sleep(1)
-                self.send_text(incoming_msg, f'Number of results set to: {num_results} \n Please wait...')
+                self.send_text(incoming_msg, f'Number of results set to: {num_results} \nPlease wait...')
                 sleep(1)
                 temp_file = search_download_youtube_video(inbound_text, num_results)
                 if isinstance(temp_file, str):
                     self.send_text(incoming_msg, temp_file)
                 else:
                     if isinstance(temp_file, list):
-                        self.send_text(incoming_msg, 'Here you go: ')
+                        self.send_text(incoming_msg, 'Here are your results: ')
                         sleep(1)
                         for file in temp_file:
                             self.send_video(incoming_msg, context, file)
@@ -111,7 +135,8 @@ if __name__ == '__main__':
     # qbot = QuoteBot(_token)
     # qbot.start()
 
-    # Starting a Youtube Bot:
+    # Define initial max number of results
     num_results = 1
+    # Starting a YouTube Bot:
     ytbot = YoutubeBot(_token)
     ytbot.start()
--- utils.py
+++ utils.py
@@ -5,30 +5,35 @@ from time import sleep
 
 def search_download_youtube_video(video_name, num_results):
     """
-    This function downloads the first num_results search results from Youtube
-    :param video_name: string of the video name
+    This function downloads the first num_results search results from YouTube
+    :param video_name: string of the video name to search
     :param num_results: integer representing how many videos to download
     :return: list of paths to your downloaded video files
     """
+    # Flag to check if file/s exist on server or download is required
     dlflag = False
+    # Parameters for youtube_dl use
     ydl = {'noplaylist': 'True', 'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4', 'outtmpl': '/./ytdlAppData/%(id)s.%(ext)s'}
-    with YoutubeDL(ydl) as ydl:
-        ydl.cache.remove()
-
-        # 1. get a list of video file names with download=false
-        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=False)['entries']
-        # for each file name in list, check duration limits, then if exists, if not download.
-        for video in videos:
-            if video['duration'] >= 900:
-                return "Error, selected track/s are above defined duration limit"
-            if video['duration'] <= 0.1:
-                return "Error, selected track/s are below defined duration limit"
-            if not (os.path.isfile(f'{ydl.prepare_filename(video)}')):
-                dlflag = True
-            # return list either way.
-        sleep(1)
-        if dlflag is True:
-            dlvideos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
-            return [ydl.prepare_filename(video) for video in dlvideos]
-        else:
-            return [ydl.prepare_filename(video) for video in videos]
+    # Try to download and return list of video/s or error msg
+    try:
+        with YoutubeDL(ydl) as ydl:
+            ydl.cache.remove()
+            # 1. get a list of video file names with download=false parameter
+            videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=False)['entries']
+            # for each file name in list, check duration limits, then if not exists, mark download required flag(dlflag = True)
+            for video in videos:
+                if video['duration'] >= 900:
+                    return "Error, selected track/s are above predefined duration limit"
+                if video['duration'] <= 0.1:
+                    return "Error, selected track/s are below predefined duration limit"
+                if not (os.path.isfile(f'{ydl.prepare_filename(video)}')):
+                    dlflag = True
+            sleep(1)
+            # Download files if required and return the list of files
+            if dlflag is True:
+                dlvideos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+                return [ydl.prepare_filename(video) for video in dlvideos]
+            else:
+                return [ydl.prepare_filename(video) for video in videos]
+    except:
+        return "Error, Server error has occured"
